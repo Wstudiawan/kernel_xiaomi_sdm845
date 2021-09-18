ea0037cbc6e39f2932ea6f28bb0e49947bdc670c
Wahyu Studiawan <manusiasampah7@gmail.com>
2021-09-18 22:39:42 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -52,6 +52,9 @@ function compile() {
     make O=out ARCH=arm64 beryllium_defconfig
     make -j$(nproc --all) O=out \
                     ARCH=arm64 \
+                    LD=ld.lld \
+                    NM=llvm-nm \
+                    AR=llvm-ar \
                     CC=clang \
                     CLANG_TRIPLE=aarch64-linux-gnu- \
                     CROSS_COMPILE=aarch64-linux-android- \
