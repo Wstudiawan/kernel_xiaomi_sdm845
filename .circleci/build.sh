b43f01a9103615eb6f7d2cdf27ec00bc4c38cad7
Wstudiawan <manusiasampah7@gmail.com>
2021-09-26 19:01:23 +0530

    edit build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -52,8 +52,8 @@ function compile() {
     make O=out ARCH=arm64 beryllium_defconfig
     make -j$(nproc --all) O=out \
               ARCH=arm64 \
-              CROSS_COMPILE=aarch64-linux-android- \
-              CROSS_COMPILE_ARM32=arm-linux-androideabi-
+              CROSS_COMPILE=aarch64-linux-gnu- \
+              CROSS_COMPILE_ARM32=arm-linux-gnueabi-
                   
 
     if ! [ -a "$IMAGE" ]; then
